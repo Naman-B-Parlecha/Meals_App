abfbbbc40708ffdbae2c9b7738cd129b07eca4d9
Naman-Parlecha <140307212+Naman-Parlecha@users.noreply.github.com>
2023-07-30 23:06:14 +0530

    Files

---
+++ lib/screens/CategoriesWidget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Pick a Category"),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                  category: category) // in this type of loop never use { } //
+          ],
+        ));
+  }
+}
